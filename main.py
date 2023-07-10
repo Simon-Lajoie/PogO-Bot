fb5eb54136db46417b7ca310708bc5aa4d84b98c
Simon-Lajoie <simonlajoie2@gmail.com>
2023-07-10 00:25:35 -0400

    Modified tasks to run according to the rate limit and changed logic for match history streak

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import asyncio
 import io
 import discord
 import random
+
+import requests
 from riotwatcher import LolWatcher, ApiError, TftWatcher
 from PIL import Image, ImageDraw, ImageFont
 from discord.ext import commands
@@ -29,7 +31,8 @@ region = 'na1'
 summoner_names_list = ["Sir Mighty Bacon", "Settupss", "Classiq", "Salsa King", "Sehnbon", "Wyatt1", "Gourish",
                        "Gabyumi", "Mii Chan", "meyst", "Limited", "Z3SIeeper", "BlackDrag", "Flames", "silvah bee",
                        "Tiny Cena", "Aàrón", "5billon", "Nappy", "KingNeptun3", "Mrs Mighty Bacon", "cpt stryder",
-                       "Goosecan", "cancerkween", "Azote", "Kovannate3", "ÇatFood","Tkipp"#,"dokudami milk", "Yazeed"
+                       "Goosecan", "cancerkween", "Azote", "Kovannate3", "ÇatFood", "Tkipp", "Skrt Skrt Skaarl",
+                       "NonMaisWallah"  # ,"dokudami milk", "Yazeed"
                        ]
 
 
@@ -75,12 +78,10 @@ def rank_to_value(tier_division_rank, lp):
 
 async def get_tft_ranked_stats():
     rankings_list = []
-    for summoner_name in summoner_names_list:
-        await asyncio.sleep(0.5)
+    for i, summoner_name in enumerate(summoner_names_list):
         # Gets account information
         logging.info(f"Making request to Riot API for summoner: {summoner_name}")
         summoner = tft_watcher.summoner.by_name(region=region, summoner_name=summoner_name)
-        await asyncio.sleep(0.5)
         # Gets TFT rankedStats using summoner's ID
         logging.info(f"Making request to Riot API for ranked stats of summoner: {summoner_name}")
         rankedStats = tft_watcher.league.by_summoner(region=region, encrypted_summoner_id=summoner["id"])
@@ -107,6 +108,13 @@ async def get_tft_ranked_stats():
             print(f" {summoner_name} : {tier}")
             tier_division_lp = tier_division
         rankings_list.append((summoner_name, ranked_value, lp, tier, tier_division_lp))
+
+        if (i + 1) % 5 == 0:
+            response = requests.get(
+                "https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/cuPjNdhPu6N3dCfsZE5bYkSPGmvBEdKMv5KUIM7ToWd1A5E?api_key=" + tft_watcher_key)
+            rate_limit_remaining = response.headers['X-App-Rate-Limit-Count']
+            logging.info(f"Rate limit remaining: {rate_limit_remaining}")
+            print(f"Rate limit remaining: {rate_limit_remaining}")
     rankings_list.sort(key=lambda x: x[1], reverse=True)
     return rankings_list
 
@@ -164,7 +172,8 @@ def get_discord_username(summoner_name):
                    "Yazeed": "<@495380694525280276>", "Kenpachi": "<@263107658762944512>",
                    "Goosecan": "<@221019724505546752>", "cancerkween": "<@999785244045615224>",
                    "azote": "<@80372982337241088>", "Kovannate3": "<@194615471226617865>",
-                   "ÇatFood": "160067484559474688"}
+                   "ÇatFood": "<@160067484559474688>", "Skrt Skrt Skaarl": "<@272440042251616256>",
+                   "NonMaisWallah": "<@520754531525459969>"}
     return discord_ids[summoner_name]
 
 
@@ -246,107 +255,107 @@ async def update_leaderboard():
     # channel = client.get_channel(1118758206840262686) TESTING SERVER
     general_channel = client.get_channel(846551161388662789)
     tft_leaderboard_channel = client.get_channel(1118278946048454726)
-    while True:
-        print("collecting ranked stats...")
-        rankings_list = await get_tft_ranked_stats()
+    #while True:
+    print("collecting ranked stats...")
+    rankings_list = await get_tft_ranked_stats()
 
-        # Compare the top 4 summoners in the previous rankings with the top 3 summoners in the new rankings
-        for i in range(4):
-            if previous_rankings and rankings_list[i][0] != previous_rankings[i][0]:
-                current_player = rankings_list[i][0]
-                previous_rank = [x for x in range(len(previous_rankings)) if previous_rankings[x][0] == current_player][
-                    0]
-                if i < previous_rank:
-                    print(rankings_list[i])
-                    print(previous_rankings[i])
-                    # Send alert message when rankings have changed and someone ranked up
-                    await general_channel.send(get_random_message(previous_rankings[i][0], rankings_list[i][0], i + 1))
-        previous_rankings = rankings_list
-        print("updating leaderboard...")
-        # Delete the last leaderboard message if it exists
-        if hasattr(update_leaderboard, "last_message") and update_leaderboard.last_message:
-            try:
-                print("deleting last message...")
-                await update_leaderboard.last_message.delete()
-            except:
-                pass
-        # Set up some constants
-        NORMAL_FONT_SIZE = 25
-        MEDIUM_FONT_SIZE = 23
-        SMALL_FONT_SIZE = 21
-        RANK_IMAGE_SIZE = (55, 55)
-        POGO_IMAGE_SIZE = (40, 40)
-        BACKGROUND_IMAGE_PATH = "img/leaderboard_background.png"  # Set the path to your background image here
-        BACKGROUND_SIZE = (1366, 757)  # Set the size of your background image here
+    # Compare the top 4 summoners in the previous rankings with the top 3 summoners in the new rankings
+    for i in range(4):
+        if previous_rankings and rankings_list[i][0] != previous_rankings[i][0]:
+            current_player = rankings_list[i][0]
+            previous_rank = [x for x in range(len(previous_rankings)) if previous_rankings[x][0] == current_player][
+                0]
+            if i < previous_rank:
+                print(rankings_list[i])
+                print(previous_rankings[i])
+                # Send alert message when rankings have changed and someone ranked up
+                await general_channel.send(get_random_message(previous_rankings[i][0], rankings_list[i][0], i + 1))
+    previous_rankings = rankings_list
+    print("updating leaderboard...")
+    # Delete the last leaderboard message if it exists
+    if hasattr(update_leaderboard, "last_message") and update_leaderboard.last_message:
+        try:
+            print("deleting last message...")
+            await update_leaderboard.last_message.delete()
+        except:
+            pass
+    # Set up some constants
+    NORMAL_FONT_SIZE = 25
+    MEDIUM_FONT_SIZE = 23
+    SMALL_FONT_SIZE = 21
+    RANK_IMAGE_SIZE = (55, 55)
+    POGO_IMAGE_SIZE = (40, 40)
+    BACKGROUND_IMAGE_PATH = "img/leaderboard_background.png"  # Set the path to your background image here
+    BACKGROUND_SIZE = (1366, 757)  # Set the size of your background image here
 
-        # Load the background image and resize it to the desired size
-        background_image = Image.open(BACKGROUND_IMAGE_PATH).convert("RGBA")
-        background_image = background_image.resize(BACKGROUND_SIZE)
+    # Load the background image and resize it to the desired size
+    background_image = Image.open(BACKGROUND_IMAGE_PATH).convert("RGBA")
+    background_image = background_image.resize(BACKGROUND_SIZE)
 
-        # Create a new image with the same size as the background image and convert it to RGBA mode
-        image = Image.new("RGB", BACKGROUND_SIZE, "white").convert("RGBA")
-        image.alpha_composite(background_image)
+    # Create a new image with the same size as the background image and convert it to RGBA mode
+    image = Image.new("RGB", BACKGROUND_SIZE, "white").convert("RGBA")
+    image.alpha_composite(background_image)
 
-        draw = ImageDraw.Draw(image)
+    draw = ImageDraw.Draw(image)
 
-        # Draw the summoner names, tier images, and tier text
-        x_offsets = [70, 513, 956]
-        y_offsets = [0, 73, 146, 219, 292, 364, 439]
-        for i in range(3):
-            for j in range(7):
-                index = i * 7 + j
-                if index >= len(rankings_list):
-                    break
-                summoner = rankings_list[index]
-                x = x_offsets[i]
-                y = y_offsets[j]
+    # Draw the summoner names, tier images, and tier text
+    x_offsets = [70, 513, 956]
+    y_offsets = [0, 73, 146, 219, 292, 364, 439]
+    for i in range(3):
+        for j in range(7):
+            index = i * 7 + j
+            if index >= len(rankings_list):
+                break
+            summoner = rankings_list[index]
+            x = x_offsets[i]
+            y = y_offsets[j]
 
-                if len(summoner[0]) > 12:
-                    # Load the font smaller size for summoner name
-                    font = ImageFont.truetype("fonts/BebasNeue-Regular.ttf", SMALL_FONT_SIZE)
-                else:
-                    # Load the font normal size for summoner name
-                    font = ImageFont.truetype("fonts/BebasNeue-Regular.ttf", NORMAL_FONT_SIZE)
-                # Draw summoner name
-                draw.text((x + 45, y + 235), summoner[0], fill="white", font=font)
-
-                # Draw tier image
-                image_path = f"img/{summoner[3]}.png"
-                tier_image = Image.open(image_path).convert("RGBA")
-                if summoner[3] == "UNRANKED":
-                    image_size = POGO_IMAGE_SIZE
-                else:
-                    image_size = RANK_IMAGE_SIZE
-                tier_image.thumbnail(image_size)
-                tier_image_x = x + 165
-                if summoner[3] == "UNRANKED" or summoner[3] == "PLATINUM" or summoner[3] == "DIAMOND" or summoner[
-                    3] == "MASTER" or summoner[3] == "GRANDMASTER" or summoner[3] == "CHALLENGER":
-                    tier_image_y = y + 225
-                else:
-                    tier_image_y = y + 220
-                image.alpha_composite(tier_image, dest=(tier_image_x, tier_image_y))
-
-                # Load the font normal size for the tier & rank text
+            if len(summoner[0]) > 12:
+                # Load the font smaller size for summoner name
+                font = ImageFont.truetype("fonts/BebasNeue-Regular.ttf", SMALL_FONT_SIZE)
+            else:
+                # Load the font normal size for summoner name
                 font = ImageFont.truetype("fonts/BebasNeue-Regular.ttf", NORMAL_FONT_SIZE)
-                # Load the font small size for the tier GM+ & rank text
-                small_font = ImageFont.truetype("fonts/BebasNeue-Regular.ttf", MEDIUM_FONT_SIZE)
-                # Draw tier & rank text
-                if summoner[3] == "GRANDMASTER" or summoner[3] == "CHALLENGER":
-                    draw.text((x + 237, y + 235), f"{summoner[4]}", fill="white", font=small_font)
-                else:
-                    draw.text((x + 237, y + 235), f"{summoner[4]}", fill="white", font=font)
+            # Draw summoner name
+            draw.text((x + 45, y + 235), summoner[0], fill="white", font=font)
 
-        # Save the image to a file-like object in memory
-        with io.BytesIO() as output:
-            image.save(output, format="PNG")
-            output.seek(0)
-            print("sending updated leaderboard...")
-            update_leaderboard.last_message = await tft_leaderboard_channel.send(
-                file=discord.File(output, filename="leaderboard.png"))
+            # Draw tier image
+            image_path = f"img/{summoner[3]}.png"
+            tier_image = Image.open(image_path).convert("RGBA")
+            if summoner[3] == "UNRANKED":
+                image_size = POGO_IMAGE_SIZE
+            else:
+                image_size = RANK_IMAGE_SIZE
+            tier_image.thumbnail(image_size)
+            tier_image_x = x + 165
+            if summoner[3] == "UNRANKED" or summoner[3] == "PLATINUM" or summoner[3] == "DIAMOND" or summoner[
+                3] == "MASTER" or summoner[3] == "GRANDMASTER" or summoner[3] == "CHALLENGER":
+                tier_image_y = y + 225
+            else:
+                tier_image_y = y + 220
+            image.alpha_composite(tier_image, dest=(tier_image_x, tier_image_y))
 
-            print("waiting 5 minutes before repeating...")
-            #Wait for 5 minutes before updating the leaderboard again
-            await asyncio.sleep(300)
+            # Load the font normal size for the tier & rank text
+            font = ImageFont.truetype("fonts/BebasNeue-Regular.ttf", NORMAL_FONT_SIZE)
+            # Load the font small size for the tier GM+ & rank text
+            small_font = ImageFont.truetype("fonts/BebasNeue-Regular.ttf", MEDIUM_FONT_SIZE)
+            # Draw tier & rank text
+            if summoner[3] == "GRANDMASTER" or summoner[3] == "CHALLENGER":
+                draw.text((x + 237, y + 235), f"{summoner[4]}", fill="white", font=small_font)
+            else:
+                draw.text((x + 237, y + 235), f"{summoner[4]}", fill="white", font=font)
+
+    # Save the image to a file-like object in memory
+    with io.BytesIO() as output:
+        image.save(output, format="PNG")
+        output.seek(0)
+        print("sending updated leaderboard...")
+        update_leaderboard.last_message = await tft_leaderboard_channel.send(
+            file=discord.File(output, filename="leaderboard.png"))
+
+           # print("waiting 5 minutes before repeating...")
+            # Wait for 5 minutes before updating the leaderboard again
+           # await asyncio.sleep(300)
 
 
 async def get_match_history(previous_match_history_ids):
@@ -355,17 +364,17 @@ async def get_match_history(previous_match_history_ids):
     for summoner_name in summoner_names_list:
         # Gets account information
         summoner = tft_watcher.summoner.by_name(region=region, summoner_name=summoner_name)
-        # Gets last 4 TFT games ids from match history using summoner's PUUID and find the ranked games
-        last_4_games_ids = tft_watcher.match.by_puuid(region=region, puuid=summoner["puuid"], count=4)
+        # Gets last 3 TFT games ids from match history using summoner's PUUID and find the ranked games
+        last_3_games_ids = tft_watcher.match.by_puuid(region=region, puuid=summoner["puuid"], count=3)
         ranked_games = []
-        for game in last_4_games_ids:
+        for game in last_3_games_ids:
             match = tft_watcher.match.by_id(region=region, match_id=game)
             if match["info"]["queue_id"] == 1100:
-                ranked_games.append(game)
+                ranked_games.append(match)
         print(f"Ranked games ID: {ranked_games}")
-        match_history_ids.append(ranked_games)
-        # Check if any ids in last_4_games_ids are present in previous_match_history_ids
-        common_ids = set(ranked_games).intersection(previous_match_history_ids)
+        match_history_ids.extend(last_3_games_ids)
+        # Check if any ids in last_3_games_ids are present in previous_match_history_ids
+        common_ids = set(match_history_ids) == set(previous_match_history_ids)
         logging.info(f"{summoner_name} common ids: {common_ids}")
         print(f"{summoner_name} common ids: {common_ids}")
         placements = []
@@ -381,26 +390,42 @@ async def get_match_history(previous_match_history_ids):
             for i in range(len(placements) - 1):
                 if placements[i] == 1 and placements[i + 1] == 1:
                     pogo_emote = "<:PogO:949833186689568768>"
-                    await general_channel.send(f"🏆 ATTENTION! {get_discord_username(summoner_name)} got two 1st placements in a row! {pogo_emote} 🏆")
+                    await general_channel.send(
+                        f"🏆 ATTENTION! {get_discord_username(summoner_name)} got two 1st placements in a row! {pogo_emote} 🏆")
                 if placements[i] == 8 and placements[i + 1] == 8:
                     aycaramba_emote = "<:aycaramba:960051307752849448>"
-                    await general_channel.send(f"📉 ATTENTION! {get_discord_username(summoner_name)} got two 8th placements in a row! {aycaramba_emote} 📉")
+                    await general_channel.send(
+                        f"📉 ATTENTION! {get_discord_username(summoner_name)} got two 8th placements in a row! {aycaramba_emote} 📉")
 
     return match_history_ids
 
 
 async def check_match_history_streak():
     previous_match_history_ids = []
+    logging.info(f"Checking match history...")
+    print(f"Checking match history...")
+    match_history_ids = await get_match_history(previous_match_history_ids)
+    # Save current match history ids
+    previous_match_history_ids = match_history_ids
+    print(f"previous match history ids: {previous_match_history_ids}")
+
+
+async def update_tasks():
     while True:
-        logging.info(f"Checking match history...")
-        print(f"Checking match history...")
-        match_history_ids = await get_match_history(previous_match_history_ids)
-        # Save current match history ids
-        previous_match_history_ids = match_history_ids
-        print(f"previous match history ids: {previous_match_history_ids}")
-        print("waiting 10 minutes before repeating...")
-         #Wait for 10 minutes before checking match history again
-        await asyncio.sleep(600)
+        # Call update_leaderboard every 5 minutes
+        client.loop.create_task(update_leaderboard())
+        await asyncio.sleep(300)
+
+        # Call check_match_history_streak every 10 minutes
+        client.loop.create_task(check_match_history_streak())
+        await asyncio.sleep(300)
+
+        client.loop.create_task(update_leaderboard())
+        await asyncio.sleep(300)
+
+        # Clear the contents of the app.log file
+        with open("app.log", "w") as log_file:
+            log_file.truncate()
 
 
 # =====================
@@ -411,9 +436,7 @@ async def on_ready():
     logging.debug("on_ready function called")
     await client.tree.sync()
     print("Success: PogO bot is connected to Discord".format(client))
-    client.loop.create_task(update_leaderboard())
-    await asyncio.sleep(150)
-    client.loop.create_task(check_match_history_streak())
+    client.loop.create_task(update_tasks())
 
 
 @client.hybrid_command()
@@ -435,5 +458,6 @@ async def on_message(message):
         await message.channel.send(file=discord.File("img/tpogo.png"))
     # if message.content.startswith('test'):
     #    await message.channel.send('test')
+
 
 client.run(client_id)
